7a021b1439c2d57b06dfc56b00750dc957819050
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-12-15 13:34:57 +0200

    change backup directory to /tmp

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 token="dropbox-token-here"
 database="database-name-here"
 timestamp=`date +%Y-%m-%d`
-backupDir="$HOME/pg_backup"
+backupDir="/tmp"
 filename="$backupDir/backup-$timestamp.sql"
 pg_dump $database > $filename
 curl -H "Authorization: Bearer $token" https://api-content.dropbox.com/1/files_put/auto/ -T $filename
