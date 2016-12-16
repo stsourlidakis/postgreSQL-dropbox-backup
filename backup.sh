4887be2cbc41cb96c93b4b0426bcde4bfda5bf20
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-12-16 16:20:30 +0200

    gzip the resulting file

--- backup.sh
+++ backup.sh
@@ -2,6 +2,6 @@
 token="dropbox-token-here"
 database="database-name-here"
 backupDir="/tmp"
-filename="$backupDir/backup-$(date +%Y-%m-%d).sql"
-pg_dump $database > $filename
+filename="$backupDir/backup-$(date +%Y-%m-%d).gz"
+pg_dump $database | gzip > $filename
 curl -H "Authorization: Bearer $token" https://api-content.dropbox.com/1/files_put/auto/ -T $filename && rm $filename || echo 'Curl returned error:'$?
